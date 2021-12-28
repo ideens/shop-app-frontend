aff3d2b507d243509c24a990552b60b7f2bf92d9
ideens <ideens@users.noreply.github.com>
2021-12-28 14:35:53 +0000

    updated proxy with the base url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
-  app.use(createProxyMiddleware('/api', { target: 'http://127.0.0.1:8000/' }))
+  app.use(
+    createProxyMiddleware('/api', { target: process.env.REACT_APP_BASE_URL })
+  )
 }
