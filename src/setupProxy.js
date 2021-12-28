efab1a6b5d3d97f0e65f0a07264bc6a9ec94c1df
ideens <ideens@users.noreply.github.com>
2021-12-28 15:37:07 +0000

    put the env variable into quuotes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('/api', { target: process.env.REACT_APP_BASE_URL })
+    createProxyMiddleware('/api', {
+      target: `${process.env.REACT_APP_BASE_URL}`,
+    })
   )
 }
